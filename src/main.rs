f6121975e838ff68f81d3b0c587f77e0503ee55d
Polo <nathan.rx@gmail.com>
2022-01-15 14:03:47 -0600

    data types for numbers, text, true and false

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,72 @@
 fn main() {
     println!("Hello, world!");
+    variables();
+    data_types();
+    true_false();
+    text_characters_strings();
+    string_example();
+}
+
+fn variables() {
+    // Declare a variable
+    let a_number;
+
+    // Declare a second variable and bind the value
+    let a_word = "Ten";
+
+    // Bind a value to the first variable
+    a_number = 10;
+
+    println!("The number is {}.", a_number);
+    println!("The word is {}.", a_word);
+}
+
+// Explore data types for numbers, text, and true/false values
+fn data_types() {
+    //
+    let number: u32 = 14;
+    print!("Then number is {}.", number);
+
+    // Addition, Subtraction, and Multiplication
+    println!(
+        "1 + 2 = {} and 8 - 5 = {} and 15 * 3 = {}",
+        1u32 + 2,
+        8i32 - 5,
+        15 * 3
+    );
+
+    // Integer and Floating point division
+    println!("9 / 2 = {} but 9.0 / 2.0 = {}", 9u32 / 2, 9.0 / 2.0);
+}
+
+fn true_false() {
+    // Declare variable to store result of "greater than" test, Is 1 > 4? -- false
+    let is_bigger = 1 > 4;
+    println!("Is 1 > 4? {}", is_bigger);
+}
+
+fn text_characters_strings() {
+    let uppercase_s = 'S';
+    let lowercase_f = 'f';
+    let smiley_face = '😃';
+}
+
+fn string_example() {
+    // Specify the data type "char"
+    let character_1: char = 'S';
+    let character_2: char = 'f';
+
+    // Compiler interprets a single item in quotations as the "char" data type
+    let smiley_face = '😃';
+
+    // Compiler interprets a series of items in quotations as a "str" data type and creates a "&str" reference
+    let string_1 = "miley ";
+
+    // Specify the data type "str" with the reference syntax "&str"
+    let string_2: &str = "ace";
+
+    println!(
+        "{} is a {}{}{}{}.",
+        smiley_face, character_1, string_1, character_2, string_2
+    );
 }
